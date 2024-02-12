c835c40a676c3cf6aaaaad20709ddb876c11880a
Danial Maqsood <daniyalmaqsood31@gmail.com>
2024-02-12 08:13:18 +0500

    finding name of habitable planets

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ fs.createReadStream('kepler_data.csv')
     })
     .on('end',()=>{
         console.log(`${habitablePlanets.length} habitable planets found`);
+        console.log(habitablePlanets.map((planet)=>{
+            return planet['kepler_name']
+
+        }));
         console.log("done");
 
     });
